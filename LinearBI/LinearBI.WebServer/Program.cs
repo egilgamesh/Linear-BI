49e23074eb775007a0aac4dcdc5a8ec2d1d79807
egilgamesh <egilgamesh@outlook.com>
2023-10-20 14:35:57 +0200

    make web server more dynamic, ready to read report content from files or database

--- LinearBI/LinearBI.WebServer/Program.cs
+++ LinearBI/LinearBI.WebServer/Program.cs
@@ -5,72 +5,58 @@ namespace LinearBI.WebServer;
 
 public class Program
 {
+	// ReSharper disable once ArrangeTypeMemberModifiers
+	// ReSharper disable once TooManyDeclarations
 	public static async Task Main(string[] args)
 	{
 		var listener = new HttpListener();
-		listener.Prefixes.Add("http://localhost:8080/"); // Set your desired URL and port
+		listener.Prefixes.Add($"http://localhost:8080/"); // Set your desired URL and port
 		listener.Start();
 		Console.WriteLine("Server is running...");
+		var routeHandlers = new Dictionary<string, Func<HttpListenerContext, Task>>
+		{
+			{ "/home", (context) => RenderContent(context, GetContent("Home"), 200) },
+			{
+				"/about",
+				(context) => RenderContent(context, "Learn about us on the About Page!", 200)
+			},
+			{ "/notfound", (context) => RenderContent(context, "404 - Not Found", 404) },
+			// Add more routes and content functions here
+		};
 		while (true)
 		{
 			var context = await listener.GetContextAsync().ConfigureAwait(false);
-			await ProcessRequestAsync(context).ConfigureAwait(false);
+			await ProcessRequestAsync(context, routeHandlers).ConfigureAwait(false);
 		}
 	}
 
-	//static async Task ProcessRequestAsync(HttpListenerContext context)
-	//{
-	//	var dynamicMessage = "Hello, Dynamic World!";
-	//	var responseString = $"<html><body><h1>{dynamicMessage}</h1></body></html>";
-	//	var buffer = Encoding.UTF8.GetBytes(responseString);
-	//	context.Response.ContentLength64 = buffer.Length;
-	//	var output = context.Response.OutputStream;
-	//	await output.WriteAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
-	//	output.Close();
-	//}
-
-	static async Task ProcessRequestAsync(HttpListenerContext context)
+	static async Task ProcessRequestAsync(HttpListenerContext context,
+		IReadOnlyDictionary<string, Func<HttpListenerContext, Task>> routeHandlers)
 	{
 		var requestUrl = context.Request.RawUrl;
-		switch (requestUrl)
+		if (routeHandlers.ContainsKey(requestUrl))
 		{
-		case "/home":
-			await RenderHomePage(context).ConfigureAwait(false);
-			break;
-		case "/about":
-			await RenderAboutPage(context).ConfigureAwait(false);
-			break;
-		default:
-			await RenderNotFoundPage(context).ConfigureAwait(false);
-			break;
+			var handler = routeHandlers[requestUrl];
+			await handler(context).ConfigureAwait(false);
+		}
+		else
+		{
+			await RenderContent(context, "404 - Not Found", 404).ConfigureAwait(false);
 		}
 	}
 
-	static async Task RenderHomePage(HttpListenerContext context)
+	static async Task RenderContent(HttpListenerContext context, string content, int statusCode)
 	{
-		var responseString = "<b>Welcome to the Home Page!</b>";
-		await SendResponse(context, responseString).ConfigureAwait(false);
-	}
-
-	static async Task RenderAboutPage(HttpListenerContext context)
-	{
-		var responseString = "Learn about us on the About Page!";
-		await SendResponse(context, responseString).ConfigureAwait(false);
-	}
-
-	static async Task RenderNotFoundPage(HttpListenerContext context)
-	{
-		context.Response.StatusCode = 404;
-		var responseString = "404 - Not Found";
-		await SendResponse(context, responseString).ConfigureAwait(false);
-	}
-
-	static async Task SendResponse(HttpListenerContext context, string responseString)
-	{
-		var buffer = Encoding.UTF8.GetBytes(responseString);
+		context.Response.StatusCode = statusCode;
+		var buffer = Encoding.UTF8.GetBytes(content);
 		context.Response.ContentLength64 = buffer.Length;
 		var output = context.Response.OutputStream;
 		await output.WriteAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
 		output.Close();
+	}
+
+	private static string GetContent(string reportName)
+	{
+		return "<b>Welcome to the Home Page!</b>";
 	}
 }
